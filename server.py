cae0a67193c6d9debb57e5e02792e5ae17609cfb
Your Name <you@example.com>
2023-07-03 22:59:46 +0530

    Moved Player class to player.py. Sending player objects between client and server unlike earlier. So also needed to modify network.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 from _thread import start_new_thread
+from player import Player
+import pickle
 
 server = "192.168.29.184"
 port = 5555
@@ -14,43 +16,33 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started...")
 
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])  # str to tuple
-
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])  # tuple to str
-
-
-pos = [(0, 0), (100, 100)]
+players = [Player(0, 0, 50, 50, (255, 0, 0)), Player(100, 100, 50, 50, (0, 0, 255))]
 
 
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps(players[player]))
     while True:
         try:
-            # receive position from player
-            data = read_pos(conn.recv(2048 * 8).decode())
+            # receive player from client
+            data = pickle.loads(conn.recv(2048))
 
             if not data:
                 print("Disconnected")
                 break
             else:
-                # update position with the received values
-                pos[player] = data
+                # update stored player with the received player
+                players[player] = data
 
-                # send the other player's position to client
+                # send the other player to client
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
 
                 print("Received : ", data)
                 print("Sending : ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
 
         except socket.error as e:
             print(e)
