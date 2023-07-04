8597f20fc4e1a17399f505e61b4e441f633e0ddb
Your Name <you@example.com>
2023-07-04 10:25:18 +0530

    Modified network.py as per needs of the game

--- network.py
+++ network.py
@@ -16,13 +16,13 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return pickle.loads(self.client.recv(2048))
+            return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
 
     def send(self, data):
         try:
-            self.client.send(pickle.dumps(data))
+            self.client.send(str.encode(data))
             return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
